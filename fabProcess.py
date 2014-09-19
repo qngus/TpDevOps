f389bb31f2191bb6f0709432ab8d82732c8ebea1
clement <levasseur.clement@gmail.com>
2014-09-19 16:35:32 +0200

    INTERNET

--- fabProcess.py
+++ fabProcess.py
@@ -3,5 +3,6 @@ from fabric.api import task
 @task
 def pull():
 	code_dir = '/var/www/TpDevOps'
-	with cd(code_dir):
+	root = '/var/www/'
+	with cd(root):
 	run(git clone https://github.com/qngus/TpDevOps.git)
